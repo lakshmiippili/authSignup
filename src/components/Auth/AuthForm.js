e48b06f0a41f28ca61ea8a686d825953d4626584
lakshmiippili <116436471+lakshmiippili@users.noreply.github.com>
2023-11-15 14:02:53 +0530

    managing token

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,13 +1,15 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
 
 import classes from "./AuthForm.module.css";
+import AuthContext from "../../store/authcontext";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const emailInputRef = useRef();
   const pswdInputRef = useRef();
 
+  const authCtx = useContext(AuthContext)
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
@@ -15,39 +17,48 @@ const AuthForm = () => {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredPswd = pswdInputRef.current.value;
-    setIsLoading(true)
-    let url
+    setIsLoading(true);
+    let url;
     if (isLogin) {
-        url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCwqSdCiKn7cgnw-tha9BdUgK-CCZyx0AU'
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCwqSdCiKn7cgnw-tha9BdUgK-CCZyx0AU";
     } else {
-      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCwqSdCiKn7cgnw-tha9BdUgK-CCZyx0AU'
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCwqSdCiKn7cgnw-tha9BdUgK-CCZyx0AU";
     }
-      fetch(
-        url,
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPswd,
-            returnSecureToken: true,
-          }),
-          headers:{
-            'Content-Type':'application/JSON'
-          }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPswd,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+    })
+      .then((response) => {
+        setIsLoading(false);
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((data) => {
+            let errorMessage = "Authentication Failed!";
+            // if(data && data.error && data.error.message){
+            //   errorMessage=data.error.message
+            // }
+            // alert(errorMessage)
+            throw new Error(errorMessage);
+          });
         }
-      ).then(response=>{
-        setIsLoading(false)
-        if(response.ok){
-          return response.json()
-        }else{
-          response.json().then(data=>{
-            let errorMessage='Authentication Failed!'
-            if(data && data.error && data.error.message){
-              errorMessage=data.error.message
-            }
-            alert(errorMessage)
-          })
-        }
+      })
+      .then((data) => {
+        authCtx.login(data.idToken)
+        console.log(data);
+        
+      })
+      .catch((err) => {
+        alert(err.message);
       });
   };
   return (
@@ -63,7 +74,9 @@ const AuthForm = () => {
           <input type="password" id="password" required ref={pswdInputRef} />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin?'Login':'Create Account'}</button>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
           {isLoading && <p>SEnding Request.....</p>}
           <button
             type="button"
